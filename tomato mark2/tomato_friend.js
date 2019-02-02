29d80460351a38852b112a1ae7bad5c182f26728
Anne-Marie <anne-marie.zaccarin@mail.mcgill.ca>
2019-02-02 14:55:21 -0500

    hello draggable friend here

--- tomato mark2/tomato_friend.js
+++ tomato mark2/tomato_friend.js
@@ -1,17 +1,45 @@
 // tomato friend javascript
 
-console.log("hello tomato");
+// Make the DIV element draggable:
 
-function allowDrop(ev) {
-	ev.preventDefault();
-}
+ var tom = document.getElementById("mydiv");
 
-function drag(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
-}
+ console.log(tom)
 
-function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(data));
+dragElement(tom);
+
+function dragElement(elmnt) {
+    //  move the DIV from anywhere inside the DIV: 
+    elmnt.onmousedown = dragMouseDown;
+
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
